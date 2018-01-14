c41bc87352ec2963123c7089fb01472ec585d35d
codegan <elbukaevzaur@gmail.ru>
2018-01-14 10:44:29 +0300

    default charset

--- data-server.js
+++ data-server.js
@@ -4,7 +4,7 @@ var sockets = [];
 
 var s = net.Server(function(socket) {
     socket.on('data', function(d) {
-            console.log(d.toString('utf-8'));
+            console.log(d);
     });
 });
 
